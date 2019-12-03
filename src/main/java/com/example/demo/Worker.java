cebc1eeb532808f46d0d9fbf8c718b016e4262c9
Piotr Rogowski <polishotaku@gmail.com>
2019-12-03 16:23:05 +0100

    Created class to store data about worker

---
+++ src/main/java/com/example/demo/Worker.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Worker {
+    private int id;
+    private String title;
+    private String givenName;
+    private String familyName;
+    private String institute;
+}
